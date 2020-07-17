bc199d4991cba07df2184bdee86e7005d2fb9fb4
Marius1388 <mariuscristian.bancila@gmail.com>
2020-07-17 14:50:01 +0300

    small bug fixed

--- src/components/stripe-button/stripe-button.component.js
+++ src/components/stripe-button/stripe-button.component.js
@@ -6,7 +6,8 @@ import StripeCheckout from 'react-stripe-checkout';
 import { clearCart } from '../../redux/cart/cart.actions';
 
 const StripeCheckoutButton = ({ price }) => {
-	const priceForStripe = parseInt(price.slice(1)) * 100;
+	// const priceForStripe = parseInt(price.slice(1)) * 100;
+	const priceForStripe = price * 100;
 	const publishableKey = 'pk_test_INy53guAQjoOPmMFPUIVZt7A004EpodQ5z';
 
 	const onToken = (token) => {
