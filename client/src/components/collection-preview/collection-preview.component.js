ca45a116efc56c0cf13b7af989d4f329995d2932
Marius1388 <mariuscristian.bancila@gmail.com>
2020-09-04 12:25:55 +0300

    backend implemented for stripe payments

---
+++ client/src/components/collection-preview/collection-preview.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component';
+
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => (
+	<div className="collection-preview">
+		<h1 className="title">{title.toUpperCase()}</h1>
+		<div className="preview">
+			{
+				// we want to show only 4 items in the preview
+				items
+					.filter((item, index) => index < 4)
+					.map((item) => (
+						<CollectionItem key={item.id} item={item} />
+					))
+			}
+		</div>
+	</div>
+);
+
+export default CollectionPreview;
---
+++ client/src/redux/store.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
+
+//set up the middleware
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === 'development') {
+	middlewares.push(logger);
+}
+
+export const store = createStore(
+	rootReducer,
+	compose(
+		applyMiddleware(...middlewares),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
+
+export default { store, persistor };
