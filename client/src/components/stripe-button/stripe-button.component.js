6e05610c42803bca38d6e6a2e5c0074db2978825
Marius1388 <mariuscristian.bancila@gmail.com>
2020-09-08 13:52:35 +0300

    clear cart update

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -6,7 +6,7 @@ import StripeCheckout from 'react-stripe-checkout';
 // import axios from 'axios';
 import { clearCart } from '../../redux/cart/cart.actions';
 
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({ price, clearCart }) => {
 	// const priceForStripe = parseInt(price.slice(1)) * 100;
 	const priceForStripe = price * 100;
 	const publishableKey = 'pk_test_INy53guAQjoOPmMFPUIVZt7A004EpodQ5z';
