234611a24d69126cd4d7c13471b6ae09ab766207
Marius1388 <mariuscristian.bancila@gmail.com>
2020-09-11 13:16:53 +0300

    server.js cache tweak

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors());
+app.disable('etag');
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'client/build')));
